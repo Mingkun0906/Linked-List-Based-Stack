051f1712fcbd808f17c2518df37b7c949e2ba85d
Mingkun Liu <125697302+Mingkun0906@users.noreply.github.com>
2023-12-20 20:00:49 -0800

    Update stacktests.cpp

--- stacktests.cpp
+++ stacktests.cpp
@@ -1,24 +1,8 @@
-#include "catch_amalgamated.hpp"
-#include "postfixCalc.hpp"
 #include "LLStack.hpp"
 #include <unordered_map>
 #include <string>
 
-// NOTE:  these are not intended as exhaustive tests.
-// This should get you started testing.
-// However, be aware that if ANY test marked "Required" fails, 
-// your score for the portion will be zero, regardless of other considerations.
-
-
 namespace{
-
-
-// And some tests for the linked list based stack and the associated calculator.
-    // VERY IMPORTANT:  if your LLStack is not templated, or if 
-    //      it is not linked-list based, your score for this project
-    //      will be zero.  Be sure your LLStack compiles and runs 
-    //      with non-numeric data types.  A char is a numeric type.
-    // 
 
 TEST_CASE("RequiredStackTest1", "[Required]")
 {
@@ -124,110 +108,5 @@ TEST_CASE("RequiredStackTest10", "[Required]")
     REQUIRE(myStack.size() == 2);
 }
 
-// YOU SHOULD ADD ADDITIONAL TEST_CASE "ERE" H"[CASES]"!! 
-
-
-
-TEST_CASE("RequiredCalcTest1", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "3", "*"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 15);
-}
-
-TEST_CASE("RequiredCalcTest2", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "3", "2", "-", "*"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 5);
-    
-}
-
-TEST_CASE("RequiredCalcTest3", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "3", "2", "-"
-    };
-    REQUIRE_THROWS( postfixCalculator(EXPRESSION) );
-    
-}
-
-// You can fail this test and still get partial credit from others that pass. 
-// Of course, I would recommend you get this one to pass, too...
-// Note that the group name is "CalcTest" and not "Required."
-TEST_CASE("AdditionalTest4", "[CalcTest]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "3", "*", "20", "*"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 300);
-}
-
-
-TEST_CASE("AdditionalTest5", "[CalcTest]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "4", "*", "2", "*"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 40);
-}
-
-TEST_CASE("AdditionalTest6", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "-"
-    };
-    REQUIRE_THROWS( postfixCalculator(EXPRESSION) );
-}
-
-TEST_CASE("AdditionalTest7", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "2", "@"
-    };
-    REQUIRE_THROWS( postfixCalculator(EXPRESSION) );
-}
-
-TEST_CASE("AdditionalTest8", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "0", "/"
-    };
-    REQUIRE_THROWS( postfixCalculator(EXPRESSION) );
-}
-
-TEST_CASE("AdditionalTest9", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "0", "5", "/"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 0);
-}
-
-TEST_CASE("AdditionalTest10", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "3", "2", "*", "*", "1", "+"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 31);
-}
-
-TEST_CASE("AdditionalTest11", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "0", "2", "*", "*", "1", "+"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 1);
-}
-
-TEST_CASE("AdditionalTest12", "[Required]")
-{
-    const std::vector<std::string> EXPRESSION = {
-        "5", "0", "+"
-    };
-    REQUIRE( postfixCalculator(EXPRESSION) == 5);
-}
 
 } // end namespace
